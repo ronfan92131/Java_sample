9345d2ed238d4ffe254a81831469d2fcc9832f22
Ron Fan <ronfan92131@gmail.com>
2018-12-17 12:47:34 -0800

    Update and rename HashMap.java to RansomNote_HashMap.java

--- RansomNote_HashMap.java
+++ RansomNote_HashMap.java
@@ -1,3 +1,5 @@
+//hackerrank ransom note question reference: 
+//https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 import java.io.*;
 import java.math.*;
 import java.security.*;
